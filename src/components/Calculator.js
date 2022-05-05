67554843d3fc7d7a91355d0c9d29e4c4b915203b
Ji Park <jiyongprk@gmail.com>
2022-05-05 19:24:00 -0400

    add more visual style and clean button layout

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -11,22 +11,20 @@ export default function Calculator() {
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
+                    <button>-</button>
                     <button>4</button>
                     <button>5</button>
                     <button>6</button>
+                    <button>+</button>
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>0</button>
-
-                    <button>-</button>
-                    <button>+</button>
                     <button>*</button>
-                    <button>/</button>
-
+                    <button>0</button>
                     <button>.</button>
-                    <button>C</button>
-                    <button className="delete-btn">DEL</button>
+                    <button>DEL</button>
+                    <button>/</button>
+                    <button className="clear-btn">CLEAR</button>
                 </div>
             </div>
         </div>
